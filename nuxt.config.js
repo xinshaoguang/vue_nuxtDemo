bc5e57f7d5d5de59b01572e019e27b1a603eb80f
xinshaoguang <58464732+xinshaoguang@users.noreply.github.com>
2020-03-22 16:16:56 +0800

    配置全局css

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,7 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css:["~assets/css/normalize.css"],
   /*
   ** Customize the progress bar color
   */
